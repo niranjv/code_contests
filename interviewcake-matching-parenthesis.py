73e8f039c338a9416d01ccc4963881defa072a5e
Niranjan <niranjanv+github@gmail.com>
2017-10-24 21:50:23 -0700

    Initial commit

---
+++ interviewcake-matching-parenthesis.py
@@ -0,0 +1,32 @@
+# URL: https://www.interviewcake.com/question/python/matching-parens
+# 
+# Description
+# "Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing."
+# Write a function that, given a sentence like the one above, along with the position of an opening parenthesis, finds the corresponding closing parenthesis.
+# Example: If the example string above is input with the number 10 (position of the first parenthesis), the output should be 79 (position of the last # parenthesis).
+
+
+
+def get_index_closing_parenthesis(s, idx_open):
+    """
+    :type s: str
+    :type idx_open: int
+    """
+
+    chars = list(s)
+    counter = 1
+    for i in range(idx_open+1, len(chars)):
+        if chars[i] == '(':
+            counter += 1
+        elif chars[i] == ')':
+            counter -= 1
+
+        if counter == 0:
+            return i
+
+
+if __name__ == "__main__":
+
+    s = "Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing."
+    idx_close = get_index_closing_parenthesis(s, 10)
+    print("Index of matching closing parenthesis = " + str(idx_close))
