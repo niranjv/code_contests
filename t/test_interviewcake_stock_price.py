4a6b03336c061cdaa654f585c5ea66da7d0766b9
Niranjan <niranjanv+github@gmail.com>
2017-10-27 01:08:45 -0700

    Split single test into multiple tests

--- t/test_interviewcake_stock_price.py
+++ t/test_interviewcake_stock_price.py
@@ -1,19 +1,28 @@
 import pytest
 import interviewcake_stock_price # Module in parent dir found via $PYTHONPATH
 
-def test_get_max_profit():
+def test_empty_list():
 
-    assert interviewcake_stock_price.get_max_profit([1,2]) == 1
-    assert interviewcake_stock_price.get_max_profit([1,1,1,1]) == 0
-    assert interviewcake_stock_price.get_max_profit([10, 7, 5, 8, 11, 9]) == 6
-    assert interviewcake_stock_price.get_max_profit(range(1, 10)) == 8
-    assert interviewcake_stock_price.get_max_profit(range(20, 1, -2)) == -2
-
-    # verify invalid input causes function to fail
     with pytest.raises(Exception) as e:
         interviewcake_stock_price.get_max_profit([])
     assert str(e.value) == "Need at least 2 prices for stock"
 
+def test_list_with_1_value():
     with pytest.raises(Exception) as e:
-        interviewcake_stock_price.get_max_profit([1])
+            interviewcake_stock_price.get_max_profit([1])
     assert str(e.value) == "Need at least 2 prices for stock"
+
+def test_list_with_2_values():
+    assert interviewcake_stock_price.get_max_profit([1,2]) == 1
+
+def test_list_with_identical_values():
+    assert interviewcake_stock_price.get_max_profit([1,1,1,1]) == 0
+
+def test_list_with_non_monotonic_values():
+    assert interviewcake_stock_price.get_max_profit([10, 7, 5, 8, 11, 9]) == 6
+
+def test_list_with_increasing_values():
+    assert interviewcake_stock_price.get_max_profit(range(1, 10)) == 8
+
+def test_list_with_decreasing_values():
+    assert interviewcake_stock_price.get_max_profit(range(20, 1, -2)) == -2
